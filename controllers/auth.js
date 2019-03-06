9711867aeb8b881bbbb1fbcb99e225a05507c850
Sourav Dey <souravde@usc.edu>
2019-03-05 22:39:11 -0800

    Add reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,13 +2,14 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const env = require('dotenv');
+const crypto = require('crypto');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
 
 env.config();
 
 const transporter = nodemailer.createTransport(sendGridTransport({
     auth: {
-        api_key:process.env.SENDGRID_API_KEY
+        api_key: process.env.SENDGRID_API_KEY
     }
 }))
 
@@ -36,8 +37,8 @@ exports.postLogin = (req, resp) => {
                             req.session.isLoggedIn = true;
                             req.session.user = user;
                             return req.session.save(err => {
-                                    return resp.redirect('/');
-                                });
+                                return resp.redirect('/');
+                            });
                         } else {
                             req.flash('error', 'Invalid email or password');
                             return resp.redirect('/login');
@@ -64,7 +65,7 @@ exports.getSignup = (req, resp) => {
     resp.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: message != undefined && message.length > 0 ? message[0] : null
+        errorMessage: message && message.length > 0 ? message[0] : null
     });
 };
 
@@ -103,6 +104,70 @@ exports.postSignup = (req, resp) => {
                     });
                 })
                 .catch(err => console.error(err));
+        })
+        .catch(err => console.error(err));
+};
+
+exports.getResetPassword = (req, resp) => {
+    const message = req.flash('error');
+    resp.render('auth/reset-password', {
+        path: '/reset-password',
+        pageTitle: 'Reset Password',
+        errorMessage: message && message.length > 0 ? message[0] : null
+    });
+};
+
+exports.postResetPassword = (req, resp) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.error(err);
+            return resp.redirect('/reset-password');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({
+                email: req.body.email
+            })
+            .then(user => {
+                if (!user) {
+                    req.flash('error', 'No account with that email found');
+                    return resp.redirect('/reset-password');
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 3600000;
+                return user.save()
+                    .then(result => {
+                        resp.redirect('/');
+                        return transporter.sendMail({
+                            to: req.body.email,
+                            from: 'shop@node-complete.com',
+                            subject: 'Password Reset',
+                            html: `
+                    <p> You requested a password reset. </p>
+                    <p> Click this <a href="http://localhost:8080/reset-password/${token}">link</a> to set a new password</p>
+                    `
+                        });
+                    });
+            })
+            .catch(err => console.error(err));
+    })
+};
+
+exports.getNewPassword = (req, resp) => {
+    const token = req.params.token;
+    User.findOne({
+            resetToken: token,
+            resetTokenExpiration: {
+                $gt: Date.now()
+            }
+        })
+        .then(user => {
+            const message = req.flash('error');
+            resp.render('auth/new-password', {
+                path: '/new-password',
+                pageTitle: 'New Password',
+                errorMessage: message && message.length > 0 ? message : null,
+                userId: user._id.toString()
+            });
         })
         .catch(err => console.error(err));
 };
--- models/user.js
+++ models/user.js
@@ -10,6 +10,8 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    resetToken: String,
+    resetTokenExpiration: Date,
     cart: {
         items: [{
             productId: {
